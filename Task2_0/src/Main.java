a087707152b974802652ea97e06ad71046431b09
Саша <genijmkx@gmail.com>
2026-09-12 11:45:57 +0300

    Добавил первые уроки Java

---
+++ Task2_0/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    int birthYear = 2007;
+    int correctYear = 2026;
+    int age = correctYear - birthYear;
+    System.out.println("В "+correctYear + " году вам будет " +age + ".");
+    }
+
+}
---
+++ task1_0/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Саша";
+        int age =19;
+        String sity = "Гомель";
+        System.out.println("Привет меня зовут " +name);
+        System.out.println("Мне " +age);
+        System.out.println("Я из города " +sity);
+
+    }
+}
